106f83a42474c06273ab5aa0b22b960d6aa42481
Julian R <ramosrodriguez123@gamil.com>
2026-01-06 17:52:40 -0500

    cambios 2

--- TXT.py
+++ TXT.py
@@ -1,3 +1,5 @@
+Nombre = input("Ingrese su nombre: ")
+print("Hola", Nombre)
 n1 = input("Ingrese un numero: ")
 n2 = input("Ingrese otro numero: ")
-print("La suma de", n1, "y", n2, "es:", int(n1) + int(n2))
+print(Nombre, "La suma de", n1, "y", n2, "es:", int(n1) + int(n2))
